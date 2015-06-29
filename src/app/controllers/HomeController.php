46752320647b05d3070b76053a41c3f2dff36bf1
zhangjiulong <kowloon29320@163.com>
2015-06-29 17:35:01 +0800

    home controller

--- src/app/controllers/HomeController.php
+++ src/app/controllers/HomeController.php
@@ -9,19 +9,19 @@ class HomeController extends Controller{
     
     public function rules() {
         return [
-            ['pass,username','required','on'=>'index'],
-            ['pass','ip','on'=>'index'],
+            //['pass,username','required','on'=>'index'],
+            //['pass','ip','on'=>'index'],
         ];
     }
     
     public function attributeLabels() {
         return array(
-            'pass'=>'密码'
+            //'pass'=>'密码'
         );
     }
     
     public function indexAction($word='world!') {
-        p($this->getRequest('pass'));
+        //p($this->getRequest('pass'));
         return 'hello,'.$word;
     }
     
