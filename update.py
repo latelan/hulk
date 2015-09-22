2d070dcf09b34fbd6773b6642c8b66274d86a354
zhangjiulong <kowloon29320@163.com>
2015-09-22 15:09:08 +0800

    update update.py

--- update.py
+++ update.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 #coding=utf8
-import httplib
 import threading
 import Queue
 import os
 import hashlib
 import subprocess
 import json
-#https://raw.githubusercontent.com/kowloon29320/hulk/master
+import time
+import sys
 
-#下载失败的文件
-error_files = []
 #新添加的文件
 new_files = []
 #修改的文件
 diff_files = []
-
+#clone的目录
+clone_dir = '/tmp/lon'+time.strftime('%Y%m%d%H%M%S')
+#git的源目录
+git_path = 'git@github.com:kowloon29320/hulk.git'
 
 #框架url
 frame_urls = [
-    'src/frame/base/ExceptionFrame.php', 
+    'src/frame/base/ExceptionFrame.php',
     'src/frame/base/FrameApp.php',
     'src/frame/base/FrameDI.php',
     'src/frame/base/FrameObject.php',
@@ -32,8 +33,8 @@ frame_urls = [
     'src/frame/console/FrameConsoleRequest.php'
 ]
 
-#组件库url                                                                                                                                                                                  
-libs_urls = [ 
+#组件库url
+libs_urls = [
     'src/libs/db/FrameDbExpression.php',
     'src/libs/db/FrameDB.php',
     'src/libs/db/FrameQuery.php',
@@ -46,12 +47,12 @@ libs_urls = [
 ]
 
 #工具urls
-utils_urls = [ 
-    'src/libs/utils/Validator.php', 
-    'src/libs/utils/ValidateTrait.php', 
-    'src/libs/utils/ArrayUtil.php', 
-    'src/libs/utils/MailUtil.php', 
-    'update.py', 
+utils_urls = [
+    'src/libs/utils/Validator.php',
+    'src/libs/utils/ValidateTrait.php',
+    'src/libs/utils/ArrayUtil.php',
+    'src/libs/utils/MailUtil.php',
+    #'update.py',
 ]
 
 urls = frame_urls + libs_urls + utils_urls
@@ -59,62 +60,49 @@ urls = frame_urls + libs_urls + utils_urls
 q = Queue.Queue(0)
 basepath = os.getcwd()
 
-def get_url(url):
-    global error_files,q,basepath
-    uri = '/kowloon29320/hulk/master/'+url.strip('/');
-    httpClient =None
-    #https://raw.githubusercontent.com/kowloon29320/hulk/master
-    try:
-        httpClient = httplib.HTTPSConnection('raw.githubusercontent.com',443,timeout=10)
-        httpClient.request('GET',uri);
-        response = httpClient.getresponse()
-        res = response.read()
-        update_file(res,url)
-
-        #print response.status
-        #print response.reason
-        #print res[20:50]
-        print url+" done..."
-    except Exception,e:
-        q.put(url)
-        #error_files.append(url) 
-        #print url+":"
-        #print e
-    finally:
-        if httpClient:
-            httpClient.close()
-
-def update_file(res,file):
-    global basepath,new_files,diff_files
-    file = file.lstrip('/')
-    filename = basepath+'/'+file
-    #print filename
-    if os.path.exists(filename):
-       with open(filename,'r') as f:
-            filestr = f.read()
-       old_md5 = hashlib.new('md5',filestr).hexdigest() 
-       new_md5 = hashlib.new('md5',res).hexdigest()
-       if old_md5 != new_md5:
-            with open(filename,'w') as f:
-                f.write(res)
-            diff_files.append(file)
-    else:
-        new_files.append(file)
-        dir = os.path.dirname(filename)
-        try:
-            subprocess.call('mkdir -p '+dir,shell=True)
-        except:
-            print 'mkdir %s fail' % dir
-        with open(filename,'w') as f:
-            f.write(res)
-
 def worker():
     while True:
         item = q.get()
-        get_url(item)
+        getFile(item)
         q.task_done()
 
+#从git克隆到本地
+def cloneFromGit():
+    global clone_dir,git_path
+    try:
+        subprocess.call('/usr/bin/git clone '+git_path+' '+clone_dir,shell=True)
+    except Exception as e:
+        print e
+        sys.exit(1)
+
+#比对处理文件
+def getFile(url):
+    global q,basepath,clone_dir,new_files,diff_files
+    src_file = clone_dir+'/'+url.strip('/')
+    filename = basepath+'/'+url.lstrip('/')
+    if os.path.exists(filename):
+        with open(filename,'r') as f:
+            filestr = f.read()
+        with open(src_file,'r') as f:
+            srcstr = f.read()
+        oldmd5 = hashlib.new('md5',filestr).hexdigest()
+        newmd5 = hashlib.new('md5',srcstr).hexdigest()
+        if oldmd5 != newmd5:
+            diff_files.append(url)
+            with open(filename,'w') as f:
+                f.write(srcstr)
+    else:
+        new_files.append(url)
+        dir = os.path.dirname(filename)
+        try:
+            subprocess.call('/bin/mkdir -p '+dir,shell=True)
+            subprocess.call('/bin/cp '+src_file+' '+filename,shell=True)
+        except Exception as e:
+            print e
+    print 'File '+url+' done!'
+
 if __name__ == '__main__':
+    cloneFromGit()
     print 'update start...'
     for i in range(len(urls)/2):
         t = threading.Thread(target=worker)
@@ -123,13 +111,11 @@ if __name__ == '__main__':
     for url in urls:
        q.put(url)
     q.join()
+    #删除克隆的目录
+    subprocess.call('/bin/rm -rf '+clone_dir,shell=True)
     msgs = {
         "new_files":new_files,
         "update_files":diff_files,
-        "error_files":error_files
     }
     print "\nResult:"
     print json.dumps(msgs,indent=2)
-    
-
-
